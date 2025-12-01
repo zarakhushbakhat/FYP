adb7af10d6589dda5c4373b5e45387aa3dd21e7e
zarakhushbakhat <zarakhushbakhat.4@gmail.com>
2025-12-01 12:37:19 +0500

    Remove some extention errors

--- Design Landing Page Prototype/src/declarations.d.ts
+++ Design Landing Page Prototype/src/declarations.d.ts
@@ -1,5 +1,25 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.gif';
-declare module '*.svg';
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
